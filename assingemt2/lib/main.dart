81cfd60b748b12c3ebe56459fc461fa9f34c53f0
siphesihle vilane <sphesihlerh12@gmail.com>
2025-05-12 21:34:27 +0200

    set up the assigmnet
    set up

--- assingemt2/lib/main.dart
+++ assingemt2/lib/main.dart
@@ -1,6 +1,24 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Firebase
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyBb8JPqXBxnF-TfO9T5ScbMe81mVtjNHNc",
+        authDomain: "tpg-assignment2-39126.firebaseapp.com",
+        projectId: "tpg-assignment2-39126",
+        storageBucket: "tpg-assignment2-39126.firebasestorage.app",
+        messagingSenderId: "1001928969727",
+        appId: "1:1001928969727:web:228b2c493a9f9377e86bca",
+      ),
+    );
+  } else
+    await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
